a260868f72fc88b5f8985f7b00962745120e55e1
Plabon Saha <sahaplabon66@gmail.com>
2023-11-06 17:15:59 +0600

    single room details show page done

--- src/Pages/checkOut.jsx
+++ src/Pages/checkOut.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import { useLoaderData } from "react-router-dom";
 
-const checkOut = () => {
+
+const CheckOut = () => {
+    const room = useLoaderData();
+    const {image,name,rating} = room
     return (
         <div>
-            
+           <h2>Room Details: {name}</h2> 
+           <div className="card w-96 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src={image} alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">{name}!</h2>
+    <p>Ratings: {rating}</p>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions">
+      <input className="btn btn-primary" type="submit" value="Book Now" />
+    </div>
+  </div>
+</div>
         </div>
     );
 };
 
-export default checkOut;
+export default CheckOut;
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -4,6 +4,7 @@ import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/Signup";
 import Rooms from "../Pages/Home/Rooms";
+import CheckOut from "../Pages/checkOut";
 
 
 const router = createBrowserRouter([
@@ -26,6 +27,11 @@ const router = createBrowserRouter([
         {
           path:'rooms',
           element:<Rooms></Rooms>
+        },
+        {
+          path: 'checkout/:id',
+          element:<CheckOut></CheckOut>,
+          loader: ({params}) => fetch(`http://localhost:5000/Rooms/${params.id}`)
         }
       ]
     },
