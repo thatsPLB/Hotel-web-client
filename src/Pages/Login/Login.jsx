d36bf74450257b7305e00fdc21f7c58571ae9b47
Plabon Saha <sahaplabon66@gmail.com>
2023-11-08 12:33:36 +0600

    Axion setup successfull

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import logimg from '../../assets/3-Login-Page-Screen.jpg'
 import { useContext } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
+import axios from 'axios';
 const Login = () => {
   const {signIn} = useContext(AuthContext)
   const location = useLocation()
@@ -13,12 +14,17 @@ const Login = () => {
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value
-        console.log(name,email,password);
         signIn(email,password)
         .then(result =>{
-          const user = result.user
-          console.log(user);
-          navigate(location?.state ? location?.state: '/')
+          const loggedInUser = result.user
+          console.log(loggedInUser);
+          const user = {email}
+          // navigate(location?.state ? location?.state: '/')
+          // get access token
+          axios.post('http://localhost:5000/jwt', user)
+          .then(res =>{
+            console.log(res.data);
+          })
         })
         .catch(error=> console.log(error))
     }
